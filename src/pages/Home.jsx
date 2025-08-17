c9f3f41a43188b36e2bf65c822464712f2a9cde6
Gaurav Bajpai <corecomputer143@gmail.com>
2025-08-17 11:07:48 +0530

    setup application

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Karigar Foundadtion</h2>
+      <p>Work in progress </p>
+    </div>
+  )
+}
+
+export default Home
